8663acf8e3d4797eae461e5fe66bda068acb12d3
marcovigano <marco.vigano@ymail.com>
2020-06-07 11:28:44 +0100

    navbar menu to block

--- js/app.js
+++ js/app.js
@@ -21,7 +21,6 @@ const sections = document.querySelectorAll(".section");
 const navMenu = document.querySelector(".navbar__menu");
 const scrollToTopButton = document.querySelector(".scroll-to-top");
 
-// build the nav
 function generateNav() {
     const navBarList = document.querySelector("#navbar__list");
     sections.forEach((element) => {
@@ -35,65 +34,65 @@ function generateNav() {
 
 function isElementInViewport(el) {
     let rect = el.getBoundingClientRect();
-    return rect.top <= ( window.innerHeight - 150 ) && rect.bottom  >= ( window.innerHeight - 150 );
+    return rect.top <= (window.innerHeight - 100) && rect.bottom >= (window.innerHeight - 100);
 }
 
 
 function addActiveIfInViewport() {
-    document.addEventListener("scroll", () => {
-        for (let i = 0; i < sections.length; i++) {
+    for (let i = 0; i < sections.length; i++) {
 
-            const section = sections[i];
+        const section = sections[i];
 
-            let currentMenuLinkActive = document.querySelector("[href='#"+section.id+"']");
-            if (isElementInViewport(section)) {
-                section.classList.add("your-active-class");
-                currentMenuLinkActive.classList.add('your-active-link');
-            } else {
-                currentMenuLinkActive.classList.remove('your-active-link');
-            }
+        let currentMenuLinkActive = document.querySelector("[href='#" + section.id + "']");
+        if (isElementInViewport(section)) {
+            section.classList.add("your-active-class");
+            currentMenuLinkActive.classList.add('your-active-link');
+        } else {
+            currentMenuLinkActive.classList.remove('your-active-link');
         }
-    });
+    }
 }
 
 let timerUserScrolling;
 
 function hideNavBar() {
-    document.addEventListener("scroll", () => {
-        navMenu.style.display = "block";
+    navMenu.style.display = "block";
 
-        if (timerUserScrolling != null) {
-            clearTimeout(timerUserScrolling)
-        }
+    if (timerUserScrolling != null) {
+        clearTimeout(timerUserScrolling)
+    }
 
-        if (window.pageYOffset > 50) {
-            timerUserScrolling = setTimeout(() => {
-                navMenu.style.display = "none";
-            }, 1000);
-        }
-    });
+    if (window.pageYOffset > 50) {
+        timerUserScrolling = setTimeout(() => {
+            navMenu.style.display = "none";
+        }, 1000);
+    }
 }
 
 function goToTopButton() {
-    document.addEventListener("scroll", () => {
-        scrollToTopButton.style.display = "none";
+    scrollToTopButton.style.display = "none";
 
-        if (window.pageYOffset > 200) {
-            scrollToTopButton.style.display = "block";
-        }
-    })
+    if (window.pageYOffset > 200) {
+        scrollToTopButton.style.display = "block";
+    }
 }
 
-function clickShowButton() {
-    scrollToTopButton.addEventListener("click", () => {
-        window.scrollTo({top: 0, behavior: 'smooth'});
-    });
+function showButton() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
 }
 
-//bottom defaulted not showing
-scrollToTopButton.style.display = "none";
-generateNav();
-addActiveIfInViewport();
-hideNavBar();
-clickShowButton();
-goToTopButton();
+
+document.addEventListener('DOMContentLoaded', () => {
+    scrollToTopButton.style.display = "none";
+    generateNav();
+});
+
+window.addEventListener('click', () => {
+    showButton();
+});
+
+window.addEventListener('scroll', () => {
+    hideNavBar();
+    goToTopButton();
+    addActiveIfInViewport();
+});
